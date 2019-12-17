b1a89b892b9d91c70a2b81b27225daa078650926
RahulRj09 <rahul18@navgurukul.org>
2019-12-17 19:26:13 +0530

    Add. login route

--- routes/customer/customer.js
+++ routes/customer/customer.js
@@ -3,5 +3,6 @@ const customer = express.Router();
 const customerController = require("../../controllers/customer");
 
 customer.post("/create", customerController.create);
+customer.post("/login", customerController.login);
 
 module.exports = customer;
