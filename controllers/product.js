5ed7a88a7a7391c9ad2ef7b803dcf37cf5265bee
RahulRj09 <rahul18@navgurukul.org>
2019-12-17 16:10:08 +0530

    Add. product/search route

--- controllers/product.js
+++ controllers/product.js
@@ -22,3 +22,24 @@ exports.getProductById = (req, res) => {
       return res.json("data not found");
     });
 };
+
+exports.searchProduct = (req, res) => {
+  const search = req.params.search;
+  knex
+    .select(
+      "product_id",
+      "name",
+      "description",
+      "price",
+      "discounted_price",
+      "thumbnail"
+    )
+    .from("product")
+    .where("name", "like", search)
+    .orWhere("name", "like", search + " %")
+    .orWhere("name", "like", "% " + search)
+    .orWhere("description", "like", "%" + search + "%")
+    .then(data => {
+      return res.json(data);
+    });
+};
--- routes/product/product.js
+++ routes/product/product.js
@@ -4,7 +4,7 @@ const productController = require("../../controllers/product");
 
 product.use(express.json());
 product.get("/", productController.getProducts);
-product.get("/search", productController.searchProduct)
+product.get("/search/:search", productController.searchProduct);
 product.get("/:product_id", productController.getProductById);
 
 module.exports = product;
