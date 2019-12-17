f35d3ae59d62de92ce05f8c7f8864428b0435e48
RahulRj09 <rahul18@navgurukul.org>
2019-12-17 18:58:26 +0530

    Add. route create

--- routes/customer/customer.js
+++ routes/customer/customer.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const customer = express.Router();
+const customerController = require("../../controllers/customer");
 
+customer.post("/create", customerController.create);
 
 module.exports = customer;
