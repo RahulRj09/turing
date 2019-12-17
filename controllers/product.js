5965590758c1297176dfc9fe6dbe365316baa773
RahulRj09 <rahul18@navgurukul.org>
2019-12-17 15:34:05 +0530

    Add. getProductById controller

--- controllers/product.js
+++ controllers/product.js
@@ -10,3 +10,15 @@ exports.getProducts = (req, res) => {
       return res.json("data not found");
     });
 };
+
+exports.getProductById = (req, res) => {
+  knex("product")
+    .select("*")
+    .where({ product_id: req.params.product_id })
+    .then(data => {
+      return res.json(data);
+    })
+    .catch(err => {
+      return res.json("data not found");
+    });
+};
