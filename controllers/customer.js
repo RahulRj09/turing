912560a9451f617f7e21225d4008cb08c53dcad3
RahulRj09 <rahul18@navgurukul.org>
2019-12-17 19:20:51 +0530

    initial commit

--- controllers/customer.js
+++ controllers/customer.js
@@ -1,3 +1,16 @@
 const knex = require("../models/knex");
 
-exports.create = (req, res) => {};
+exports.create = (req, res) => {
+  knex("customer")
+    .insert({
+      name: req.body.name,
+      email: req.body.emailId,
+      password: req.body.password
+    })
+    .then(success => {
+      return res.send("Created customer");
+    })
+    .catch(error => {
+      return res.send(error);
+    });
+};
