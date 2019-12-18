a0fc6305e68be42d8658b283457813cc97890363
RahulRj09 <rahul18@navgurukul.org>
2019-12-18 10:33:54 +0530

    Add. route getUserInfo using emailid

--- routes/customer/customer.js
+++ routes/customer/customer.js
@@ -4,5 +4,6 @@ const customerController = require("../../controllers/customer");
 
 customer.post("/create", customerController.create);
 customer.post("/login", customerController.login);
+customer.get("/getUserInfo/:emailid", customerController.getUserInfo)
 
 module.exports = customer;
